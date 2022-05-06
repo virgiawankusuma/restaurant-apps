9d8e1e711b788028393120e31fb592a18489ba71
zettacamp-virgiawan-kusuma <virgiawan.kusuma@zettacamp.pro>
2022-05-06 10:14:21 +0700

    feat: finished make navigation bar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,20 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
-console.log('Hello Coders! :)');
+const nav = document.querySelector("#nav");
+
+window.addEventListener("scroll", () => {
+    const scrollY = window.scrollY;
+    if (scrollY < 20) {
+        nav.classList.remove("shadow-bottom");
+    } else {
+        nav.classList.add("shadow-bottom");
+    }
+});
+
+const menu = document.querySelector(".menu");
+const menuButton = document.querySelector(".menu-button");
+
+menuButton.addEventListener("click", () => {
+    menu.classList.toggle("open");
+});
